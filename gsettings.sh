bb3e0d7ad0ee2108411d0ffb9871c2005c51eb67
Trey Hunner <trey@treyhunner.com>
2017-07-22 00:20:36 -0700

    Add more settings after re-install

---
+++ gsettings.sh
@@ -0,0 +1,49 @@
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
+gsettings set org.gnome.desktop.wm.preferences theme 'Radiance'
+gsettings set org.gnome.desktop.interface icon-theme 'ubuntu-mono-light'
+gsettings set org.gnome.desktop.interface gtk-theme 'Radiance'
+gsettings set org.gnome.desktop.wm.keybindings minimize ['<Control><Alt>KP_0']
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-7 @as []
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-8 @as []
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-9 @as []
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-5 @as []
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-6 @as []
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 @as []
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-11 @as []
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-12 @as []
+gsettings set org.gnome.desktop.wm.keybindings unmaximize ['<Control><Super>Down']
+gsettings set org.gnome.desktop.wm.keybindings raise ['disabled']
+gsettings set org.gnome.desktop.wm.keybindings maximize ['<Control><Super>Up']
+gsettings set org.gnome.desktop.wm.keybindings maximize-horizontally ['disabled']
+gsettings set org.gnome.desktop.wm.keybindings lower ['disabled']
+gsettings set org.gnome.desktop.wm.keybindings toggle-shaded ['<Control><Alt>s']
+gsettings set org.gnome.desktop.wm.keybindings maximize-vertically ['disabled']
+gsettings set org.gnome.desktop.wm.keybindings show-desktop ['<Control><Super>d']
+gsettings set org.gnome.desktop.wm.keybindings toggle-maximized ['<Control><Alt>KP_5']
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 @as []
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 @as []
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 @as []
+gsettings set com.canonical.indicator.datetime show-day true
+gsettings set com.canonical.indicator.datetime show-date true
+gsettings set com.canonical.indicator.datetime time-format '24-hour'
+gsettings set com.canonical.indicator.datetime show-auto-detected-location true
+gsettings set org.gnome.desktop.background secondary-color '#000000'
+gsettings set org.gnome.desktop.background primary-color '#000000'
+gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/TCP118v1_by_Tiziano_Consonni.jpg'
+gsettings set org.gnome.settings-daemon.peripherals.keyboard numlock-state 'on'
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click false
+gsettings set org.gnome.desktop.peripherals.touchpad speed -0.25882352941176467
+gsettings set org.gnome.desktop.peripherals.mouse speed -0.62941176470588234
+gsettings set com.canonical.indicator.power show-time true
+gsettings set com.canonical.Unity integrated-menus false
+gsettings set com.canonical.Unity always-show-menus true
+gsettings set org.gnome.desktop.peripherals.mouse speed -0.62941176470588234
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click false
+gsettings set org.gnome.desktop.peripherals.touchpad speed -0.25882352941176467
+gsettings set com.canonical.Unity.Launcher favorites ['application://org.gnome.Nautilus.desktop', 'application://google-chrome.desktop', 'application://gnome-terminal.desktop', 'application://org.gnome.Software.desktop', 'application://nylas-mail.desktop', 'application://quasselclient.desktop', 'application://unity-sound-panel.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']
+gsettings set org.gnome.power-manager info-history-type 'charge'
+gsettings set org.gnome.power-manager info-stats-type 'discharge-accuracy'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
+gsettings set org.gnome.desktop.session idle-delay uint32 600
