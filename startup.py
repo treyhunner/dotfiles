889c42e039d4b0a357a042ebe401d5f5beb2c4bc
Trey Hunner <trey@treyhunner.com>
2025-10-09 13:22:38 -0700

    Keep PYTHONSTARTUP simple 🧹

--- startup.py
+++ startup.py
@@ -1,67 +1,41 @@
 """
 Trey's Python startup file
 
-This file adds keyboard shortcuts to the 3.13+ pyrepl.
-It also customizes REPL syntax highlighting in 3.14+.
+This file adds keyboard shortcuts to pyrepl (3.13+).
+It also customizes REPL syntax highlighting (3.14+).
 
 To use this file:
 
-Put this in ~/.zshenv or ~/.zshrc:
+Put this in your ~/.zshenv, ~/.zshrc, ~/.bashrc, etc.:
 export PYTHONSTARTUP=$HOME/.startup.py
 
 Run this:
 mkdir -p $HOME/.pyhacks
 python -m pip install pyrepl-hacks --target=$HOME/.pyhacks
 """
+import pathlib as _pathlib, sys as _sys
+_sys.path.append(str(_pathlib.Path.home() / ".pyhacks"))
 
-
-def _main():
-    try:
-        _pyrepl_hacks()
-    except ImportError:
-        pass  # Python 3.12 or below or pyrepl_hacks isn't in ~/.pyhacks
-
-    # Fun stuff, in case rich is installed
-    try:
-        import rich.pretty
-    except ImportError:
-        pass
-    else:
-        rich.pretty.install()
-
-
-def _pyrepl_hacks():
-    """Customizations powered by pyrepl-hacks."""
-    from pathlib import Path
-    import sys
-    sys.path.append(str(Path.home() / ".pyhacks"))
-    import pyrepl_hacks as repl  # Raises ImportError on 3.12 & below
-
-    repl.bind("Alt+M", "move-to-indentation")
-    repl.bind("Shift+Tab", "dedent")
-    repl.bind("Alt+Down", "move-line-down")
-    repl.bind("Alt+Up", "move-line-up")
-    repl.bind("Shift+Home", "home")
-    repl.bind("Shift+End", "end")
-
-    repl.bind_to_insert("Ctrl+N", "[2, 1, 3, 4, 7, 11, 18, 29]")
-    repl.bind_to_insert("Ctrl+F", '["apples", "oranges", "bananas", "strawberries", "pears"]')
-    repl.bind_to_insert("Ctrl+W", 'with open(')
-    repl.bind_to_insert("Ctrl+S", 'self.')
-    repl.bind_to_insert("Ctrl+P", 'print(')
-    repl.bind_to_insert("Ctrl+T", 'class ')
-    repl.bind_to_insert("Ctrl+M", 'import ')
-
-    @repl.bind(r"Ctrl+X Ctrl+R", with_event=True)
-    def subprocess_run(reader, event_name, event):
-        reader.insert("import subprocess\n")
-        code = 'subprocess.run("", shell=True)'
-        reader.insert(code)
-        for _ in range(len(code) - code.index('""') - 1):
-            repl.commands.left(reader, event_name, event)
+try:
+    import pyrepl_hacks as _repl
+except ImportError:
+    pass  # We're on Python 3.12 or below
+else:
+    _repl.bind("Home", "home")
+    _repl.bind("End", "end")
+    _repl.bind("Alt+M", "move-to-indentation")
+    _repl.bind("Shift+Tab", "dedent")
+    _repl.bind("Alt+Down", "move-line-down")
+    _repl.bind("Alt+Up", "move-line-up")
+    _repl.bind_to_insert("Ctrl+N", "[2, 1, 3, 4, 7, 11, 18, 29]")
+    _repl.bind_to_insert(
+        "Ctrl+F",
+        '["apples", "oranges", "bananas", "strawberries", "pears"]',
+    )
 
     try:
-        repl.update_theme(
+        # Solarized Light theme to match vim
+        _repl.update_theme(
             keyword="green",
             builtin="blue",
             comment="intense blue",
@@ -75,9 +49,4 @@ def _pyrepl_hacks():
     except ImportError:
         pass  # We're on Python 3.13 or below
 
-
-# Kick it all off
-_main()
-
-# Delete the global variables we defined
-del _main, _pyrepl_hacks
+    del _repl, _pathlib, _sys  # Avoid global REPL namespace pollution
